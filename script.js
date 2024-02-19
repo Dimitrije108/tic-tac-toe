61701cd582a51dc49679171d76709577e9e14064
Dimitrije Markovic <126205228+Dimitrije108@users.noreply.github.com>
2024-02-19 17:07:27 +0100

    Add functionality to create the gameboard

---
+++ script.js
@@ -0,0 +1,67 @@
+function Gameboard() {
+    const board = [];
+    const row = 3;
+    const column = 3;
+
+    for (let i = 0; i < row; i++) {
+        board[i] = [];
+        for (let j = 0; j < column; j++) {
+            board[i].push(j);
+        }
+    }
+
+    const getBoard = () => board;
+
+    return getBoard();
+}
+
+// function Play() {
+//     const board = Gameboard();
+
+//     const player1 = {
+//         name: this.name,
+//         value: "X",
+//     }
+//     const player2 = {
+//         name: this.name,
+//         value: "O",
+//     }
+
+//     const getBoard = () => board;
+
+//     let activePlayer = player1;
+
+//     function changePlayerTurn() {
+//         activePlayer = activePlayer === player1 ? player2 :  player1;
+//     }
+
+//     function playRound(row, column) {
+//         if (typeof board[row][column] !== 'string') {
+//             board[row][column] = activePlayer.value;
+//             changePlayerTurn();
+//         }
+//         winCondition();
+//     }
+
+//     function winCondition() {
+//         for (let i = 0; i < board.length; i++) {
+//             for (let j = 0; j < i.length; j++) {
+//                 if (typeof board[i][j] !== 'string') {
+//                     break;
+//                 } else {
+//                     alert("It's a draw!");
+//                 }
+//             }
+//         }
+//     }
+
+//     return {
+//         playRound,
+//         getBoard,
+//     }
+// }
+
+// const play = Play();
+//player objects
+
+//control the flow of the game object
