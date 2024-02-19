bd675224e4cd3ac0bcc11cc2f3f9bfbbf3a3981e
Dimitrije Markovic <126205228+Dimitrije108@users.noreply.github.com>
2024-02-19 17:09:58 +0100

    Add functionality to play a round and change players turn

--- script.js
+++ script.js
@@ -15,53 +15,54 @@ function Gameboard() {
     return getBoard();
 }
 
-// function Play() {
-//     const board = Gameboard();
+function Play() {
+    const board = Gameboard();
 
-//     const player1 = {
-//         name: this.name,
-//         value: "X",
-//     }
-//     const player2 = {
-//         name: this.name,
-//         value: "O",
-//     }
+    const player1 = {
+        name: this.name,
+        value: "X",
+    }
+    const player2 = {
+        name: this.name,
+        value: "O",
+    }
 
-//     const getBoard = () => board;
+    const getBoard = () => board;
 
-//     let activePlayer = player1;
+    let activePlayer = player1;
 
-//     function changePlayerTurn() {
-//         activePlayer = activePlayer === player1 ? player2 :  player1;
-//     }
+    function changePlayerTurn() {
+        activePlayer = activePlayer === player1 ? player2 :  player1;
+    }
 
-//     function playRound(row, column) {
-//         if (typeof board[row][column] !== 'string') {
-//             board[row][column] = activePlayer.value;
-//             changePlayerTurn();
-//         }
-//         winCondition();
-//     }
+    function playRound(row, column) {
+        if (typeof board[row][column] !== 'string') {
+            board[row][column] = activePlayer.value;
+            changePlayerTurn();
+        }
+        winCondition();
+    }
 
-//     function winCondition() {
-//         for (let i = 0; i < board.length; i++) {
-//             for (let j = 0; j < i.length; j++) {
-//                 if (typeof board[i][j] !== 'string') {
-//                     break;
-//                 } else {
-//                     alert("It's a draw!");
-//                 }
-//             }
-//         }
-//     }
+    function winCondition() {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < i.length; j++) {
+                if (typeof board[i][j] !== 'string') {
+                    break;
+                } else {
+                    alert("It's a draw!");
+                }
+            }
+        }
+    }
 
-//     return {
-//         playRound,
-//         getBoard,
-//     }
-// }
+    return {
+        playRound,
+        getBoard,
+    }
+}
 
-// const play = Play();
+const play = Play();
+
 //player objects
 
 //control the flow of the game object
